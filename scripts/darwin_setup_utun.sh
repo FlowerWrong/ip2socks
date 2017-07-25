75a11a16a2f9ec60a47d6c572798ec069d2008e5
yang <yangkang@liveneeq.com>
2017-07-25 19:28:59 +0800

    Fix darwin setup utun scripts

--- scripts/darwin_setup_utun.sh
+++ scripts/darwin_setup_utun.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 SOCKS_SERVER=$1 # SOCKS 服务器的 IP 地址
 SOCKS_PORT=1080 # 本地SOCKS 服务器的端口
 
 local_tun_ip=10.0.0.1
 remote_tun_ip=10.0.0.1
-dns_server=8.8.8.8
-# server=$1
 intf=utun7
 mtu=1500
 
@@ -18,11 +16,13 @@ echo $orig_gw
 
 # configure IP address and MTU of VPN interface
 ifconfig $intf $local_tun_ip $remote_tun_ip mtu $mtu netmask 255.255.255.0 up
+echo $SOCKS_SERVER
+echo $orig_gw
 route add -net $SOCKS_SERVER $orig_gw
 
 # 国内网段走家用网关（路由器）的 IP 地址
-for i in $(cat "$CHINA_IP"); do
-    ip route add "$i" via "$GATEWAY_IP"
+for i in $(cat $CHINA_IP); do
+    route add -net $i $orig_gw
 done
 
 # change routing table
