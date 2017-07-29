fc7db51ad89b833fba4724309f6274fedc3ff6dd
yang <yangkang@liveneeq.com>
2017-07-29 23:43:32 +0800

    Add libyaml cmake

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 #include "ev.h"
+#include "yaml.h"
 
 #include "lwip/init.h"
 #include "lwip/mem.h"
@@ -123,6 +124,76 @@ main(int argc, char **argv) {
   strncpy(nm_str, ip4addr_ntoa(&netmask), sizeof(nm_str));
   strncpy(gw_str, ip4addr_ntoa(&gw), sizeof(gw_str));
   printf("Host at %s mask %s gateway %s\n", ip_str, nm_str, gw_str);
+
+
+
+
+
+
+
+
+  /**
+   * yaml
+   */
+  FILE *fh = fopen("/Users/kingyang/dev/java/lipu/ip2socks/scripts/config.example.yml", "r");
+  yaml_parser_t parser;
+  yaml_event_t  event;   /* New variable */
+
+  /* Initialize parser */
+  if(!yaml_parser_initialize(&parser))
+    fputs("Failed to initialize parser!\n", stderr);
+  if(fh == NULL)
+    fputs("Failed to open file!\n", stderr);
+
+  /* Set input file */
+  yaml_parser_set_input_file(&parser, fh);
+
+  /* START new code */
+  do {
+    if (!yaml_parser_parse(&parser, &event)) {
+      printf("Parser error %d\n", parser.error);
+      exit(EXIT_FAILURE);
+    }
+
+    switch(event.type)
+    {
+      case YAML_NO_EVENT: puts("No event!"); break;
+            /* Stream start/end */
+      case YAML_STREAM_START_EVENT: puts("STREAM START"); break;
+      case YAML_STREAM_END_EVENT:   puts("STREAM END");   break;
+            /* Block delimeters */
+      case YAML_DOCUMENT_START_EVENT: puts("<b>Start Document</b>"); break;
+      case YAML_DOCUMENT_END_EVENT:   puts("<b>End Document</b>");   break;
+      case YAML_SEQUENCE_START_EVENT: puts("<b>Start Sequence</b>"); break;
+      case YAML_SEQUENCE_END_EVENT:   puts("<b>End Sequence</b>");   break;
+      case YAML_MAPPING_START_EVENT:  puts("<b>Start Mapping</b>");  break;
+      case YAML_MAPPING_END_EVENT:    puts("<b>End Mapping</b>");    break;
+            /* Data */
+      case YAML_ALIAS_EVENT:  printf("Got alias (anchor %s)\n", event.data.alias.anchor); break;
+      case YAML_SCALAR_EVENT: printf("Got scalar (value %s)\n", event.data.scalar.value); break;
+    }
+    if(event.type != YAML_STREAM_END_EVENT)
+      yaml_event_delete(&event);
+  } while(event.type != YAML_STREAM_END_EVENT);
+  yaml_event_delete(&event);
+  /* END new code */
+
+  /* Cleanup */
+  yaml_parser_delete(&parser);
+  fclose(fh);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   /* lwip/src/core/init.c */
