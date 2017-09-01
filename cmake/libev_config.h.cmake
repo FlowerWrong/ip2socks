01bf6d0e18baad8ef851edac11c0200173a75ee3
yang <yangkang@liveneeq.com>
2017-09-01 14:28:40 +0800

    Update libev cmake

--- cmake/libev_config.h.cmake
+++ cmake/libev_config.h.cmake
@@ -1,11 +1,10 @@
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
+/* config.h : Generated from config.h.cmake.in in CMakeLists.txt by cmake.  */
 
 /* Define to 1 if you have the `clock_gettime' function. */
 #cmakedefine HAVE_CLOCK_GETTIME 1
 
 /* Define to 1 to use the syscall interface for clock_gettime */
-/* #undef HAVE_CLOCK_SYSCALL */
+#cmakedefine HAVE_CLOCK_SYSCALL 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #cmakedefine HAVE_DLFCN_H 1
@@ -27,10 +26,9 @@
 
 /* Define to 1 if you have the `kqueue' function. */
 #cmakedefine HAVE_KQUEUE 1
-/* #undef HAVE_KQUEUE */
 
 /* Define to 1 if you have the `rt' library (-lrt). */
-/* #undef HAVE_LIBRT */
+#cmakedefine HAVE_LIBRT 1
 
 /* Define to 1 if you have the <memory.h> header file. */
 #cmakedefine HAVE_MEMORY_H 1
@@ -40,21 +38,21 @@
 
 /* Define to 1 if you have the `poll' function. */
 #cmakedefine HAVE_POLL 1
-/* #undef HAVE_POLL */
 
 /* Define to 1 if you have the <poll.h> header file. */
 #cmakedefine HAVE_POLL_H 1
-/* #undef HAVE_POLL_H */
 
 /* Define to 1 if you have the `port_create' function. */
-/* #undef HAVE_PORT_CREATE */
+#cmakedefine HAVE_PORT_CREATE 1
 
 /* Define to 1 if you have the <port.h> header file. */
-/* #undef HAVE_PORT_H */
+#cmakedefine HAVE_PORT_H 1
 
 /* Define to 1 if you have the `select' function. */
 #cmakedefine HAVE_SELECT 1
-/* #undef HAVE_SELECT */
+
+/* Define to 1 if you have the <winsock.h> header. */
+#cmakedefine HAVE_WINSOCK_H 1
 
 /* Define to 1 if you have the `signalfd' function. */
 #cmakedefine HAVE_SIGNALFD 1
@@ -78,14 +76,13 @@
 #cmakedefine HAVE_SYS_EVENTFD_H 1
 
 /* Define to 1 if you have the <sys/event.h> header file. */
-/* #undef HAVE_SYS_EVENT_H */
+#cmakedefine HAVE_SYS_EVENT_H 1
 
 /* Define to 1 if you have the <sys/inotify.h> header file. */
 #cmakedefine HAVE_SYS_INOTIFY_H 1
 
 /* Define to 1 if you have the <sys/select.h> header file. */
 #cmakedefine HAVE_SYS_SELECT_H 1
-/* #undef HAVE_SYS_SELECT_H */
 
 /* Define to 1 if you have the <sys/signalfd.h> header file. */
 #cmakedefine HAVE_SYS_SIGNALFD_H 1
@@ -101,31 +98,31 @@
 
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
    */
-#cmakedefine LT_OBJDIR ".libs/"
+#define LT_OBJDIR "@libtool_objdir@"
 
 /* Name of package */
-#cmakedefine PACKAGE "libev"
+#define PACKAGE "@package_name@"
 
 /* Define to the address where bug reports for this package should be sent. */
-#cmakedefine PACKAGE_BUGREPORT ""
+#define PACKAGE_BUGREPORT "@package_bugreport@"
 
 /* Define to the full name of this package. */
-#cmakedefine PACKAGE_NAME ""
+#define PACKAGE_NAME ""
 
 /* Define to the full name and version of this package. */
-#cmakedefine PACKAGE_STRING ""
+#define PACKAGE_STRING ""
 
 /* Define to the one symbol short name of this package. */
-#cmakedefine PACKAGE_TARNAME ""
+#define PACKAGE_TARNAME ""
 
 /* Define to the home page for this package. */
-#cmakedefine PACKAGE_URL ""
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#cmakedefine PACKAGE_VERSION ""
+#define PACKAGE_VERSION ""
 
 /* Define to 1 if you have the ANSI C header files. */
 #cmakedefine STDC_HEADERS 1
 
 /* Version number of package */
-#cmakedefine VERSION "4.24"
+#define VERSION "@version_major@.@version_minor@"
